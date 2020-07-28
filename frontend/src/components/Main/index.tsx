2a94b75a5bf25a7975b0ced7a8c46daae4e774ac
= <=>
2020-07-28 00:08:34 -0300

    add styled components and global style

--- frontend/src/components/Main/index.tsx
+++ frontend/src/components/Main/index.tsx
@@ -1,7 +1,20 @@
+import * as STL from './styles';
+
 const Main = () => (
-  <main>
-    <h1>Won Games</h1>
-  </main>
+  <STL.Wrapper>
+    <STL.Logo
+      src="/img/logo.svg"
+      alt="Imagem de um atómo - escrito ao lado Won Games"
+    />
+    <STL.Title>Won Games</STL.Title>
+    <STL.Description>
+      TypeScript, ReactJS, NextJS, Styled Components
+    </STL.Description>
+    <STL.Logo
+      src="/img/hero-illustration.svg"
+      alt="Desenvolvedor na frente do comptutador"
+    />
+  </STL.Wrapper>
 );
 
 export default Main;
---
+++ frontend/src/pages/_app.tsx
@@ -0,0 +1,25 @@
+import type { AppProps /*, AppContext */ } from 'next/app';
+import Head from 'next/head';
+
+import GlobalStyles from 'styles/Global';
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    //substitui div vazia
+    <>
+      <Component {...pageProps} />;
+      <Head>
+        <title>Won Games</title>
+        <link rel="shortcut icon" href="/img/icon-512.png" />
+        <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <meta
+          name="description"
+          content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
+        />
+      </Head>
+      <GlobalStyles />
+    </>
+  );
+}
+
+export default App;
